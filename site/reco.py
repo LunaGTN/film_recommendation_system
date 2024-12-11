e415ba0aa5fb523e14591bae44040e97eba4e7b3
Julien Castellano <julien@MacBook-Pro-de-Julien.local>
2024-12-11 17:02:37 +0100

    reco ok

--- site/reco.py
+++ site/reco.py
@@ -1,9 +1,25 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_parquet("/Users/julien/cinema/df_film.parquet")
+df_poster = pd.read_csv('/Users/julien/cinema/df_reco.csv')
+df_poster.to_parquet('/Users/julien/cinema/df_poster.parquet')
+
+df_reco = pd.read_csv('/Users/julien/cinema/resultat.csv')
+df_reco.to_parquet('/Users/julien/cinema/df_reco.parquet')
 
 
-option = st.text_input("Search for a movie title")
-filtered_titles = df[df["titre"].str.contains(option, case=False, na=False)]["titre"]
-st.selectbox("Movies", filtered_titles)
+
+
+option = st.selectbox(
+    "Movies",
+    options=df_reco["titre"],
+    format_func=lambda x: x if st.session_state.get("search_query", "").lower() in x.lower() else None,index=None
+)
+
+resultat = df_reco.loc[df_reco["titre"] == option]
+st.dataframe(resultat)
+recos = list(resultat[["r1","r2","r3","r4","r5"]].values)[0]
+st.dataframe(df_poster.iloc[recos])
+
+
+
