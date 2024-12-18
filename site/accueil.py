702e522d26e462e5ca31cc199f78ed738f908315
LunaGTN <gayton.marinho@gmail.com>
2024-12-18 14:57:54 +0100

    Nettoyage & débug

--- site/accueil.py
+++ site/accueil.py
@@ -14,55 +14,55 @@ if "page" not in st.session_state:
     st.session_state.page = "accueil"  # Par défaut, la page d'accueil est affichée
 
     # Définition d'une fonction pour changer la page active
-    def set_page(page_name):
-        st.session_state.page = page_name  # Modification de la variable de session pour changer de page
+def set_page(page_name):
+    st.session_state.page = page_name  # Modification de la variable de session pour changer de page
 
-    # Configuration de la barre de navigation avec plusieurs colonnes
-    col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1])  # Distribution des colonnes
-    with col2:
-        if st.button("Accueil", use_container_width=True):  # Bouton Accueil
-            set_page("accueil")  # Change la page en 'accueil' lorsque ce bouton est pressé
-    with col3:
-        if st.button("Visualisations", use_container_width=True):  # Bouton Visualisations
-            set_page("viz")  # Change la page en 'viz' lorsque ce bouton est pressé
-    with col4:
-        if st.button("Recommandation", use_container_width=True):  # Bouton Recommandation
-            set_page("reco")  # Change la page en 'reco' lorsque ce bouton est pressé
+# Configuration de la barre de navigation avec plusieurs colonnes
+col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1])  # Distribution des colonnes
+with col2:
+    if st.button("Accueil", use_container_width=True):  # Bouton Accueil
+        set_page("accueil")  # Change la page en 'accueil' lorsque ce bouton est pressé
+with col3:
+    if st.button("Visualisations", use_container_width=True):  # Bouton Visualisations
+        set_page("viz")  # Change la page en 'viz' lorsque ce bouton est pressé
+with col4:
+    if st.button("Recommandation", use_container_width=True):  # Bouton Recommandation
+        set_page("reco")  # Change la page en 'reco' lorsque ce bouton est pressé
 
-    # Ajout d'un espacement entre la navigation et le contenu de la page
-    st.markdown("<div style='margin-top: 30px;'></div>", unsafe_allow_html=True)
+# Ajout d'un espacement entre la navigation et le contenu de la page
+st.markdown("<div style='margin-top: 30px;'></div>", unsafe_allow_html=True)
 
-    # Affichage de différentes pages en fonction de la variable `st.session_state.page`
-    if st.session_state.page == "accueil":
-        # Page d'accueil : Introduction et explication du site
-        st.markdown("""
-            <style>
-            @import url('https://fonts.googleapis.com/css2?family=Henny+Penny&display=swap');
+# Affichage de différentes pages en fonction de la variable `st.session_state.page`
+if st.session_state.page == "accueil":
+    # Page d'accueil : Introduction et explication du site
+    st.markdown("""
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Henny+Penny&display=swap');
+        
+        /* Style pour le titre */
+        h1 {
+            text-align: center;
+            color: white;
+            font-family: 'Henny Penny', serif;
+            font-size: 80px;
+            margin-bottom: 70px;
+        }
+        
+        /* Style pour le texte */
+        p {
+            text-align: center;
+            font-size: 20px;
+            color: white;
+        }
             
-            /* Style pour le titre */
-            h1 {
-                text-align: center;
-                color: white;
-                font-family: 'Henny Penny', serif;
-                font-size: 80px;
-                margin-bottom: 70px;
-            }
-            
-            /* Style pour le texte */
-            p {
-                text-align: center;
-                font-size: 20px;
-                color: white;
-            }
-                
-            /* Style pour les paragraphes plus détaillés */
-            div {
-                font-size: 18px;
-                color: white;
-                text-align: justify;
-            }
-            </style>
-        """, unsafe_allow_html=True)
+        /* Style pour les paragraphes plus détaillés */
+        div {
+            font-size: 18px;
+            color: white;
+            text-align: justify;
+        }
+        </style>
+    """, unsafe_allow_html=True)
 
     # Titre principal de la page d'accueil
     st.markdown(f"""<h1>Nearest Movies</h1>""", unsafe_allow_html=True)  # Affiche le titre "Nearest Movies" avec une taille de police personnalisée
@@ -86,9 +86,9 @@ if "page" not in st.session_state:
     """, unsafe_allow_html=True)  # Affichage du texte explicatif détaillant les fonctionnalités du site avec des marges personnalisées
 
 elif st.session_state.page == "viz":
-    # Page de visualisation : Appel de la fonction pour afficher les visualisations
+# Page de visualisation : Appel de la fonction pour afficher les visualisations
     afficher_viz()
 
 elif st.session_state.page == "reco":
-    # Page de recommandations : Appel de la fonction pour afficher les recommandations
+# Page de recommandations : Appel de la fonction pour afficher les recommandations
     afficher_reco()
